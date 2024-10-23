8eb6fdf72cfe26313d6e73b53bb3e40a7cc9db1e
wasee-sun <revengersun@gmail.com>
2024-10-23 22:44:44 +0600

    Added core app

---
+++ app/app/tests.py
@@ -0,0 +1,19 @@
+""" Sample test """
+
+from django.test import TestCase
+from app import calc
+
+class CalcTests(TestCase):
+    """ Test calc module """
+
+    def test_add_numbers(self):
+        """ Test that two numbers are added together """
+        res = calc.add(5, 6)
+        
+        self.assertEqual(res, 11)
+        
+    def test_subtract_numbers(self):
+        """ Test that two numbers are subtracted """
+        res = calc.subtract(10, 5)
+        
+        self.assertEqual(res, 5)
