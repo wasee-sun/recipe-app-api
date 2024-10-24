6ea64b536380e7bcbc2a721bae37274034e08f9e
wasee-sun <revengersun@gmail.com>
2024-10-24 19:10:54 +0600

    Added admin funcs and api docs

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -19,3 +19,35 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """ Test email is normalized for new users """
+        sample_emails = [
+            ['test1@EXAMPLE.com', 'test1@example.com'],
+            ['Test2@example.com', 'Test2@example.com'],
+            ['TEST3@EXAMPLE.com', 'TEST3@example.com'],
+            ['XOjzA@example.com', 'XOjzA@example.com'],
+        ]
+        for email, expected in sample_emails:
+            user = get_user_model().objects.create_user(
+                email=email,
+                password='sample123'
+            )
+            self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        """ Test that creating a user without an email raises a ValueError """
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(
+                email='',
+                password='sample123'
+            )
+
+    def test_create_superuser(self):
+        """Test Creating a superuser"""
+        user = get_user_model().objects.create_superuser(
+            email='test@example',
+            password='testpass123'
+        )
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
